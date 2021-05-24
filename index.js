a04435dc54f10b392dff182d672a5b1f0dd4427d
Jarad Solomon <jaradsolomon@gmail.com>
2021-05-24 00:31:16 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+
+function divide(x,y){
+    Number.parseInt(x, 10);
+    Number.parseInt(y, 10);
+    const div = x/y;
+    return div;
+};
+
+*/
+
+let num1 = 100;
+let num2 = 2000;
+const divide = function() {
+    return num2 / num1;
+}
+divide();
+let para;
+const square = para => para * para;
+
+const add = (num1,num2) => num1 + num2;
